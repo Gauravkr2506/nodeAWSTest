5b09899d6cc8672cf2d68bb8aa05b08a14191a56
gauravkr2506 <gauravkr2506@gmail.com>
2021-02-12 19:33:51 +0530

    changes

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const app = require("express")();
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
+// const http = require("http").Server(app);
+// const io = require("socket.io")(http);
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8081;
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello world2</h1>");
 });
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
-});
+// io.on("connection", (socket) => {
+//   console.log("a user connected");
+// });
 
-http.listen(port, () => {
+app.listen(port, () => {
   console.log("listening on *:" + port);
 });

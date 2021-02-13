a5866a2513f52992548f76619060e98e9da5a262
gauravkr2506 <gauravkr2506@gmail.com>
2021-02-14 00:05:32 +0530

    changes

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const express = require("express");
-const app = express();
 const port = process.env.PORT || 8081;
+var http = require("http");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(+port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+//create a server object:
+http
+  .createServer(function (req, res) {
+    res.write("Hello World!"); //write a response to the client
+    res.end(); //end the response
+  })
+  .listen(port); //the server object listens on port 8080

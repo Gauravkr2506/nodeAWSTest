9462f6f56e985858fecb72e13b08bf6b5b35cf4f
gauravkr2506 <gauravkr2506@gmail.com>
2021-02-13 23:46:48 +0530

    changes

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || "8081";
+const port = process.env.PORT || 8081;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
+app.listen(+port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
